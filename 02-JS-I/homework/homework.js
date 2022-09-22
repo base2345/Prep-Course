dd69725b91182dd5c349a8995f6977d7aea35237
base2345 <wsgwilliam2557@gmail.com>
2022-09-22 18:18:52 -0500

    cambios de prueva2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -3,7 +3,7 @@
 const { stringify } = require("git-url-parse");
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "william";
+const nuevaString = "porfinhoyuuuu";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 27;
